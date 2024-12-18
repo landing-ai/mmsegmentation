2a1789e527219a7522217545b4329090ff35c9f2
Dayanne Fernandes <dayannefernandesc@gmail.com>
2024-12-18 15:59:25 -0300

    fix: MixVisionTransformer by replacing EfficientMultiheadAttention with VanillaAttentionWithSRRatio (#2)
    * VanillaAttentionWithSRRatio
    
    * rm mvtv2

--- mmseg/models/backbones/mit.py
+++ mmseg/models/backbones/mit.py
@@ -88,6 +88,85 @@ class MixFFN(BaseModule):
         return identity + self.dropout_layer(out)
 
 
+# replacement for EfficientMultiheadAttention
+class VanillaAttentionWithSRRatio(BaseModule):
+    def __init__(
+        self,
+        embed_dims,
+        num_heads,
+        attn_drop=0.,
+        proj_drop=0.,
+        dropout_layer=None,  # Ignoring this! The user has no reason to change the dropout layer
+        init_cfg=None,
+        batch_first=True,  # This should always be true for this drop-in
+        qkv_bias=False,
+        norm_cfg=dict(type='LN'),
+        sr_ratio=1,
+    ):
+        super().__init__(init_cfg=init_cfg)
+        assert (
+            embed_dims % num_heads == 0
+        ), f"embed_dims {embed_dims} should be divided by num_heads {num_heads}."
+
+        if not batch_first:
+            raise NotImplementedError("For this drop-in we only support batch_first format. Sorry.")
+
+        self.embed_dims = embed_dims
+        self.num_heads = num_heads
+        head_dim = embed_dims // num_heads
+        self.scale = head_dim**-0.5  # Use a hard-coded scale to match the API from mmseg - no reason to change this
+
+        self.q = nn.Linear(embed_dims, embed_dims, bias=qkv_bias)
+        self.kv = nn.Linear(embed_dims, embed_dims * 2, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(embed_dims, embed_dims)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(embed_dims, embed_dims, kernel_size=sr_ratio, stride=sr_ratio)
+            self.norm = build_norm_layer(norm_cfg, embed_dims)[1]
+
+    def forward(self, x, hw_shape, identity=None):
+        H, W = hw_shape
+        B, N, C = x.shape
+        q = (
+            self.q(x)
+            .reshape(B, N, self.num_heads, C // self.num_heads)
+            .permute(0, 2, 1, 3)
+        )
+
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = self.norm(x_)
+            kv = (
+                self.kv(x_)
+                .reshape(B, -1, 2, self.num_heads, C // self.num_heads)
+                .permute(2, 0, 3, 1, 4)
+            )
+        else:
+            kv = (
+                self.kv(x)
+                .reshape(B, -1, 2, self.num_heads, C // self.num_heads)
+                .permute(2, 0, 3, 1, 4)
+            )
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        if identity is not None:
+            x = x + identity  # Identity is really the skip connection
+
+        return x
+
+
 class EfficientMultiheadAttention(MultiheadAttention):
     """An implementation of Efficient Multi-head Attention of Segformer.
 
@@ -258,7 +337,7 @@ class TransformerEncoderLayer(BaseModule):
         # The ret[0] of build_norm_layer is norm name.
         self.norm1 = build_norm_layer(norm_cfg, embed_dims)[1]
 
-        self.attn = EfficientMultiheadAttention(
+        self.attn = VanillaAttentionWithSRRatio(
             embed_dims=embed_dims,
             num_heads=num_heads,
             attn_drop=attn_drop_rate,
